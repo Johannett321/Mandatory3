cef2725685f3829fa2c1534f1d627519ca71e417
johannett321 <johan.svartdal30@gmail.com>
2022-10-17 10:55:20 +0200

    Improvements

--- main.py
+++ main.py
@@ -2,25 +2,28 @@ from packages.warehouse import *
 from packages.storage_manager import *
 from packages.business import *
 
-def readCmd():
+
+def read_cmd():
     command = input("Command: ")
     command_parts = command.split(" ")
     if command_parts[0] == "product":
         if command_parts[1] == "add":
             print("Added product")
 
-    readCmd()
+    read_cmd()
+
 
 def run_setup():
     print("Running setup")
-    businessName = input("Name of business: ")
-    initialBalance = input("Initial balance (money): ")
-    business = Business(businessName, initialBalance)
+    business_name = input("Name of business: ")
+    initial_balance = input("Initial balance (money): ")
+    business = Business(business_name, initial_balance)
     save_business(business)
+
 
 if load_data() is False:
     print("No data could be found")
     run_setup()
     exit(0)
 
-readCmd()
+read_cmd()
